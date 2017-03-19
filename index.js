6fd3ccc52ae41c6a755213ca29e74f46a8b1328d
Hao Zhang <zhanghao@Haos-MacBook-Pro.local>
2017-03-19 14:10:08 +0800

    mac commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index2.html');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
